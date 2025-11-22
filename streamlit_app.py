bf75714d2e7a0100e65a7b441097b1f938f1f19d
anita-lyubenova <anitaencheva@gmail.com>
2025-11-22 13:55:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,7 +140,8 @@ def process_elevations(location, radius):
 
 def show_map():
     #folium_static(st.session_state.map)
-    st_folium(st.session_state.map)
+    map_data = st_folium(st.session_state.map, key="main_map", height=600)
+    return map_data
 
 # def click_button():
 #     st.session_state.clicked = True
@@ -594,8 +595,8 @@ with tab_map:
                 - **>12%**: Very steep, strenuous; may be difficult for vehicles and bicycles
             
                 """)
-       #show_map()
-       map_data = st_folium(st.session_state.map, key="my_map", height=600)
+       map_data = show_map()
+       
        if st.session_state.nearest_poi is not None and not st.session_state.nearest_poi.empty:
            st.dataframe(st.session_state.nearest_poi)
        
