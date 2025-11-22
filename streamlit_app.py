0dc03c5c2b41b2e19db598870efc63ee531d5c63
anita-lyubenova <anitaencheva@gmail.com>
2025-11-22 11:15:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -172,7 +172,7 @@ def get_landuse_data(location, radius, tags):
    
     return pie_data0
 
-@st.cache_data
+
 def aggregate_landuse_data(landuse_data):
     pie_data = landuse_data.groupby(["pie_cat"]).agg(
         total_area_m2 = ("area_m2", "sum"),
